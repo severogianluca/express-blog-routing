5fc759924d6ed2fb90c36e6f29b8f336a647c88f
severogianluca <gianlucasevero300@gmail.com>
2025-03-27 18:29:26 +0100

    Feat: create rest crud

--- routers/posts.js
+++ routers/posts.js
@@ -3,14 +3,32 @@ const router = express.Router()
 
 const posts = require("../postsList")
 
-//LISTA
+//LISTA INDEX
 router.get('/', (req, res) => {
     console.log('sei dentro la bacheca')
     res.json(posts)
 })
 
+router.get('/:slug', (req, res) => {
+    console.log('stai prendendo l/oggetto ' + req.params.slug)
 
+    res.send('stai prendendo l/oggetto ' + req.params.slug)
+})
 
+router.post('/', (req, res) => {
+    console.log('hai creato l/oggetto ' + req.params)
+    res.send('hai creato l/oggetto ' + req.params)
+})
+
+router.put('/:slug', (req, res) => {
+    console.log('stai modificando l/oggetto ' + req.params.slug)
+    res.send('stai modificando l/oggetto ' + req.params.slug)
+})
+
+router.delete('/:slug', (req, res) => {
+    console.log('stai eliminando l/oggetto ' + req.params.slug)
+    res.send('stai eliminando l/oggetto ' + req.params.slug)
+})
 
 
 
